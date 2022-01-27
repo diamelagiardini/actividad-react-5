cf66f28d3ddb740cd2f0075f764593c6f3f6e2fb
diamelagiardini <diamelagiardini23@gmail.com>
2022-01-26 21:06:55 -0300

    primer commit

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# Getting Started with Create React App
+# 101-9na
+
+## Ejercitacion
+
+Agregar las variables de estado y funciones de evento necesarias para que, 
+
+1. Al seleccionar un pais, el pais seleccionado se vea en la web
+
+2. Al seleccionar un animal entre Perro o Gato, la seleccion se vea en la web
+
+## Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+
+// const [color, setColor] = useState("rojo");
+// const [mensajeDelBoton, setMensajeDelBoton] = useState("Poner fondo azul")
+
+// const handleClick = () => {
+//   color === "rojo" ? setColor("azul") : setColor("rojo");
+//   mensajeDelBoton === "Poner fondo rojo" 
+//     ? setMensajeDelBoton("Poner fondo azul") 
+//     : setMensajeDelBoton("Poner fondo rojo") 
+// }
+
+
+// - form  
+//   - input 
+//   - checkbox
+//   - select 
+//   - radio 
+//   - submit
+// - boton 
+
+// - cualquier elemento que escuche un evento 
+
+  const [valorDelInput, setValorDelInput] = useState("")
+  const [valorDelCheckbox, setValorDelCheckbox] = useState("NO")
+  const [valorDelSelect, setValorDelSelect ] = useState(" ")
+  const [valorDelRadio, setValorDelRadio] = useState(" ")
+
+  const handleChange = (e) => {
+    setValorDelInput(e.target.value)
+  }
+
+  const handleChangeCheckbox = (e) => {
+    e.target.checked 
+      ? setValorDelCheckbox("SI") 
+      : setValorDelCheckbox("NO")
+  }
+
+  const handleChangeSelected = (e) => {
+    setValorDelSelect(e.target.value)
+    console.log("Estas seleccionando un pais")
+  }
+
+  const handleChangeRadio = (e) => {
+    setValorDelRadio(e.target.value)
+    console.log("Estas seleccionando un animal")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {/* <div className={color}>
+      <button onClick={handleClick}>{mensajeDelBoton}</button>
+    </div> */}
+
+    <input type="text" name="nombre" onChange={handleChange}/>
+
+    <p>Valor del input: <span>{valorDelInput}</span></p>
+
+<label>Terminos y condiciones
+    <input type="checkbox" name="terminos" onChange={handleChangeCheckbox}/>
+  </label>
+
+  <p>Acepta terminos y condiciones: <span>{valorDelCheckbox}</span></p>
+
+  <label>Pais
+    <select onChange={handleChangeSelected }>
+      <option value="Argentina">Argentina</option>
+      <option value="Brasil">Brasil</option>
+      <option value="Chile">Chile</option>
+    </select>
+  </label>
+
+  <p>Pais elegido: <span>{valorDelSelect}</span></p>
+
+  <label>
+    <input onChange={handleChangeRadio} type="radio" name="animal" value="Gato" />Gato
+    <input onChange={handleChangeRadio} type="radio" name="animal" value="Perro" />Perro
+  </label>
+
+  <p>Animal elegido: <span>{valorDelRadio}</span></p>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
